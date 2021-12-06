b3acbbd88e78386d53c88b8858c872cbcb8562a4
winismyname <95560334+winismyname@users.noreply.github.com>
2021-12-06 13:18:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo bae ! 🖐🖐🖐',
+    introDesc: `Mập có điều này muốn hỏi Bé, Bé nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK Mập',
+    title: 'RY có yêu Mập không ? 😙',
+    desc: '💘 ',
+    btnYes: 'Có, RY yêu Mập 💖💖💖',
+    btnNo: '💔 Có con cac ! 💔 ',
+    question:'Nói cho Mập biết đi vì sao RY yêu Mập 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì RY yêu Mập vãi lồn 😊😊',
+    mess: 'Mập biết mà, Mập cũng yêu RY lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Mập nha. 💬💬',
+    btnAccept: 'Dạ vâng 💖',
+    messLink: 'https://m.me/profile.php?id=100030641494178'
+}
